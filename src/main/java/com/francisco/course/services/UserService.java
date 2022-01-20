8a8009079d724b1d66f935bb6770bd0ca12b9991
frberti <berti.francisco@gmail.com>
2022-01-20 09:58:16 -0300

    Service layer, component registration

---
+++ src/main/java/com/francisco/course/services/UserService.java
@@ -0,0 +1,27 @@
+package com.francisco.course.services;
+
+import com.francisco.course.entities.User;
+import com.francisco.course.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> findAll(){
+        List<User> list = userRepository.findAll();
+        return list;
+    }
+
+    public User findById(Long id){
+        Optional<User> opt = userRepository.findById(id);
+        return opt.get();
+    }
+
+}
